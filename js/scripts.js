2c0fcf04fe818b2af64744bb49f1c998e7aacd10
Demarillac Izere <izered3@gmail.com>
2020-09-25 17:37:08 +0200

    adding variables for the cc,year,month and date

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ var boysNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var girlsNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 function myFunction(){
     var date=document.getElementById('dob').value;
-    
+    date=date.split('-');
+    var year=date[0];
+    var cc=parseInt((year.substring(0,2)));
+    var yy=parseInt((year.substring(2)));
+    var month=parseInt(date[1]);
+    var day=parseInt(date[2]);
 }
