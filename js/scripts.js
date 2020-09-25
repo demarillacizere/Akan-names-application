65c052da06783c460fedb848744be6a7e3df03a9
Demarillac Izere <izered3@gmail.com>
2020-09-25 17:32:49 +0200

    adding a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
 var boysNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 var girlsNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+function myFunction(){
+    var date=document.getElementById('dob').value;
+    
+}
